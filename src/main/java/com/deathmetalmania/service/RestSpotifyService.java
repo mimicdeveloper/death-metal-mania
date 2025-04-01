ea34edab136d2810a8d1e4ca05ea4c6344216850
Yon Chong <yonchong31@gmail.com>
2025-03-31 22:44:54 -0400

    fixed restspotifyservice to searchbandsbydeathmetalgenre so token isn't shown in url

--- src/main/java/com/deathmetalmania/service/RestSpotifyService.java
+++ src/main/java/com/deathmetalmania/service/RestSpotifyService.java
@@ -38,11 +38,12 @@ public class RestSpotifyService implements SpotifyService {
 
     @Override
     public SpotifyApi searchDeathMetalBands() {
-        String url = BASE_URL + "search?q=genre%3Adeathmetal&type=artist&limit=50&access_token=" + spotifyAccessToken;
+        String url = BASE_URL + "search?q=genre%3Adeathmetal&type=artist&limit=50";
 
         RestClient restClient = RestClient.create();
         SpotifyApi fullResults = restClient.get()
                 .uri(url)
+                .header("Authorization", "Bearer " + spotifyAccessToken) // Add token in the Authorization header
                 .retrieve()
                 .body(SpotifyApi.class);
 
@@ -52,6 +53,7 @@ public class RestSpotifyService implements SpotifyService {
 
         return fullResults;
     }
+
 
     @Override
     public BandDetails getBandById(String spotifyId) {
