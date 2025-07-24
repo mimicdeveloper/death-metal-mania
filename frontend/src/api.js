d904b9fd2995db35bf09caba67bcd6d19e848f7a
Yon Chong <yonchong31@gmail.com>
2025-07-24 19:42:55 -0400

    updated env vars and updated api.js for koyeb backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_REMOTE_API, // pulls from .env file
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  baseURL: import.meta.env.VITE_REMOTE_API || 'http://localhost:9000',
 });
 
 export default api;
