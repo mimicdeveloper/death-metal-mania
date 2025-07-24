4c40f82c101b4941ef19bafff96b67b6abbf6883
Yon Chong <yonchong31@gmail.com>
2025-07-24 19:03:02 -0400

    reconfigured for koyeb backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/death-metal-mania/', // ✅ Add this line for GitHub Pages
   plugins: [vue()],
   define: {
-    // quiet warning about value not being explicitly defined
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
   },
   resolve: {
