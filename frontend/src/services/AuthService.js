1b9104b514192129e0d657c0be26dbcdcd3a75cb
Yon Chong <yonchong31@gmail.com>
2025-07-24 16:59:06 -0400

    authservice updated

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,12 +1,8 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://tragic-greta-mimicdev-d365cdd2.koyeb.app',
-});
+import api from '../api';
 
 export default {
   register(user) {
-    return api.post('/register', user); // exact match with backend mapping
+    return api.post('/register', user);
   },
   login(user) {
     return api.post('/login', user);
