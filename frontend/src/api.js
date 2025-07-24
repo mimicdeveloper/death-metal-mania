f35c1119245733f0a8ec9fd272b8020a10955e72
Yon Chong <yonchong31@gmail.com>
2025-07-23 21:44:07 -0400

    updated for koyeb deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.MODE === 'production'
-    ? 'https://tragic-greta-mimicdev-d365cdd2.koyeb.app'
-    : 'http://localhost:9000',
+  baseURL: import.meta.env.VITE_REMOTE_API || 'http://localhost:9000',
 });
 
 export default api;
