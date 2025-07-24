06c54dcc211eff41a66ccf91bc0a6540c7c67071
Yon Chong <yonchong31@gmail.com>
2025-07-23 22:28:21 -0400

    updated authservice for koyeb

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,12 +1,14 @@
-// services/AuthService.js
+import axios from 'axios';
 
-import api from '../api.js';  // your configured Axios instance
+const api = axios.create({
+  baseURL: 'https://tragic-greta-mimicdev-d365cdd2.koyeb.app',
+});
 
 export default {
+  register(user) {
+    return api.post('/register', user); // exact match with backend mapping
+  },
   login(user) {
     return api.post('/login', user);
-  },
-  register(user) {
-    return api.post('/register', user);
   }
-}
+};
