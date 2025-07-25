1457a3d0aa4b6b3ebebe7b7cdf2b759016040c0a
Yon Chong <yonchong31@gmail.com>
2025-07-24 23:32:03 -0400

    trying to get koyeb work with frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_REMOTE_API,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
-
-// Optional: Automatically attach token if available
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: import.meta.env.VITE_REMOTE_API || 'http://localhost:9000',
 });
 
 export default api;
