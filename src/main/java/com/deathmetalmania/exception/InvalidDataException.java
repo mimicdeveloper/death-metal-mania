716510fe436534f677b4fb3ded369e720a049bb5
Yon Chong <yonchong31@gmail.com>
2025-07-26 14:59:24 -0400

    Remove old root src folder, keep only server/backend/src

--- src/main/java/com/deathmetalmania/exception/InvalidDataException.java
+++
@@ -1,8 +0,0 @@
-package com.deathmetalmania.exception;
-
-public class InvalidDataException extends RuntimeException {
-
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
